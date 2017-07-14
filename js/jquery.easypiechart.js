cc3585c1dbf6d8aff7565781c514f3f7485690df
cdoyle45 <caseydoyle64@gmail.com>
2017-07-14 14:16:28 -0400

    bigger header button, change skill circles to purple

--- js/jquery.easypiechart.js
+++ js/jquery.easypiechart.js
@@ -199,7 +199,7 @@ var CanvasRenderer = function(el, options) {
 
 var EasyPieChart = function(el, opts) {
 	var defaultOptions = {
-		barColor: '#ff675f',
+		barColor: '#47376d',
 		trackColor: '#e1e1e3',
 		scaleColor: '#e1e1e3',
 		scaleLength: 0,
